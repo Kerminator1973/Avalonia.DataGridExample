66eaf829ccfd16c45e8fce76a80b90d226a2bacf
Maksim Rozhkov <kermit@mail.ru>
2023-02-28 16:03:53 +0300

    The DataGrid has appended

---
+++ Models/Banknote.cs
@@ -0,0 +1,10 @@
+﻿namespace Avalonia.DataGridExample.Models
+{
+    public class Banknote
+    {
+        public int Id { get; set; }
+        public string Currency { get; set; } = null!;
+        public string Denomination { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Avalonia.DataGridExample.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia.DataGridExample.Models;
+
+namespace Avalonia.DataGridExample.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ObservableCollection<Banknote> Banknotes { get; }
+
+    public MainWindowViewModel()
+    {
+        this.Banknotes = new ObservableCollection<Banknote>(new List<Banknote> {
+            new Banknote { Id = 1, Currency = "EUR", Denomination = "20", Status="" },
+            new Banknote { Id = 2, Currency = "EUR", Denomination = "50", Status="" },
+            new Banknote { Id = 3, Currency = "EUR", Denomination = "100", Status="Rejected" },
+            new Banknote { Id = 4, Currency = "USD", Denomination = "100", Status="" },
+            new Banknote { Id = 5, Currency = "USD", Denomination = "0", Status="Jammed" }
+        });
+    }
 }
