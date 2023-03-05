57e84fe968487b6372191a766b51ab83eecc10ec
Maksim Rozhkov <kermit@mail.ru>
2023-03-05 18:04:10 +0300

    Used a convertor to make the the Background attribute dependant of a Status field's value

---
+++ Converters/StatusConverter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace Avalonia.DataGridExample.Converters
+{
+    public class StatusConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var status = value as string;
+
+            return status switch
+            {
+                "Rejected" => Brushes.Red,
+                _ => Brushes.Green
+            };
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
