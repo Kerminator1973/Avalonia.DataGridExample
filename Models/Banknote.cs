58ec696c4e5829967f28d2846362dca73bf1d4de
Maksim Rozhkov <kermit@mail.ru>
2024-12-17 21:41:59 +0300

    #2: INotifyPropertyChanged in the model has been implemented

--- Models/Banknote.cs
+++ Models/Banknote.cs
@@ -1,10 +1,32 @@
-﻿namespace Avalonia.DataGridExample.Models
+﻿using System.ComponentModel;
+
+namespace Avalonia.DataGridExample.Models
 {
-    public class Banknote
+    public class Banknote : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Currency { get; set; } = null!;
         public string Denomination { get; set; } = null!;
-        public string Status { get; set; } = null!;
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
